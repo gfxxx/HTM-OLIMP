14ac262dc621665a72ae806c7d4336881bb95e01
Guiroux <fassotguillaume@gmail.com>
2022-08-31 19:40:48 +0200

    sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,8 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst-parser']
+source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
 exclude_patterns = []
--- docs/source/config.py
+++
@@ -1,1 +0,0 @@
-extensions = ['myst-parser']
